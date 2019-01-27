a399931d5d3bc3b2209e7e4eaf5655a582a4cebc
Nicole Li <nicolehcli@gmail.com>
2019-01-26 18:50:38 -0800

    Add fulfillment code

---
+++ firebaseFulfillment/firebase/functions/index.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const functions = require('firebase-functions')
+const { dialogflow } = require('actions-on-google')
+
+const app = dialogflow()
+
+const bills = [
+    {
+        'type': 'Phone Bill',
+        'amount': 30.00,
+        'duedate': 'January 2, 2019'
+    },
+    {
+        'type': 'Rent' ,
+        'amount': 100.00,
+        'duedate': 'January 13, 2019'
+        
+    },{
+        'type': 'Cable Bill',
+        'amount': 40.00,
+        'duedate': 'January 24, 2019'
+        
+    }]
+
+app.intent('bill.dueDate', conv => {
+    results = "Here are your bills. ";
+    for (let i = 0; i < bills.length; i++) { 
+        results += `${bills[i].type} is due on ${bills[i].duedate}. `
+    }
+    conv.ask(results)
+})
+
+app.intent('Default Fallback Intent', conv => {
+  conv.ask(`I didn't understand`)
+  conv.ask(`I'm sorry, can you try again?`)
+})
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
